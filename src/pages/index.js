ca31adf6a57039f35c4f928c25a7c81b595799e7
eldersoon <eldersondagama@hotmail.com>
2022-07-29 17:08:49 -0300

    commit aleatorio

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div>alteração de elderson</div>
+  return <div>altera ção inválida</div>
 }
