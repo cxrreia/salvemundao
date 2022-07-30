ee18f018808b76e0dea244e17f94e7c4ad126ec5
eldersoon <eldersondagama@hotmail.com>
2022-07-30 18:13:17 -0300

    table component

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import TableComponent from '../../components/TableComponent'
 
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein }
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+]
+
 export default function List() {
   return (
     <div>
-      <TableComponent />
+      <TableComponent data={rows} />
     </div>
   )
 }
