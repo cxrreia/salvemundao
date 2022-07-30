f1505c59a09900262987a69478301fb122644057
eldersoon <eldersondagama@hotmail.com>
2022-07-30 15:25:01 -0300

    transition teste in page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import ArrowForward from '@mui/icons-material/ArrowForward'
 import CardComponent from '../components/CardComponent'
+import Slide from '@mui/material/Slide'
+import { Box } from '@mui/material'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -8,11 +11,21 @@ export default function Home() {
         padding: '50px',
       }}
     >
-      <CardComponent
-        title='Usuarios'
-        text='Aqui você pode gerenciar os usuários do sistema.'
-        icon={<ArrowForward />}
-      />
+      <Slide
+        direction='left'
+        in={true}
+        mountOnEnter
+        unmountOnExit
+        timeout={3000}
+      >
+        <Box>
+          <CardComponent
+            title='Usuarios'
+            text='Aqui você pode gerenciar os usuários do sistema.'
+            icon={<ArrowForward />}
+          />
+        </Box>
+      </Slide>
     </div>
   )
 }
