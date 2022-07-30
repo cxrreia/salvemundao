8c74ac59211defe02bae9092f2fd59dd7f2e28d7
eldersoon <eldersondagama@hotmail.com>
2022-07-30 16:00:09 -0300

    table component

---
+++ src/pages/list/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TableComponent from '../../components/TableComponent'
+
+export default function List() {
+  return (
+    <div>
+      <TableComponent />
+    </div>
+  )
+}
